0244c843a816f8b47f76e90f1e00a0969f8d14b6
Joseph Kan <josephkan14@gmail.com>
2023-02-01 14:50:17 -0800

    test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-java -cp ".;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestDocSearch.java
+javac -cp ".;lib/junit-4.13.2.jar;hamcrest-core-1.3.jar" TestDocSearch.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestDocSearch
